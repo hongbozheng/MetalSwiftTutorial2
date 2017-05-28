52aca0d98c62db956ff155ed1cff7a890948a4da
danny <danny@rainbe.com>
2017-05-27 17:11:42 -0700

    finish tutorial 2, set up a series of matrix transformations to move to full 3D.

--- MetalSwift/Shader.metal
+++ MetalSwift/Shader.metal
@@ -6,18 +6,42 @@
 //  Copyright © 2017 Danny. All rights reserved.
 //
 
-//https://www.raywenderlich.com/146414/metal-tutorial-swift-3-part-1-getting-started
 
 #include <metal_stdlib>
 using namespace metal;
 
-vertex float4 basic_vertex(
-const device packed_float3 * vertex_array [[buffer(0)]],
-unsigned int vid [[vertex_id]]) {
-    return float4(vertex_array[vid],1.0);
+struct Uniforms{
+    float4x4 modelMatrix;
+    float4x4 projectionMatrix;
+};
+
+struct VertexIn{
+    packed_float3 position;
+    packed_float4 color;
+};
+
+struct VertexOut{
+    float4 position [[position]];
+    float4 color;
+};
+
+vertex VertexOut basic_vertex(
+    const device VertexIn * vertex_array [[buffer(0)]],
+    const device Uniforms& Uniforms [[buffer(1)]],
+    unsigned int vid [[vertex_id]]) {
+    
+    float4x4 mv_Matrix = Uniforms.modelMatrix;
+    float4x4 proj_Matrix = Uniforms.projectionMatrix;
+    
+    VertexIn VertexIn = vertex_array[vid];
+    VertexOut VertexOut;
+    VertexOut.position = proj_Matrix * mv_Matrix * float4(VertexIn.position,1);
+    VertexOut.color = VertexIn.color;
+    
+    return VertexOut;
 }
 
 
-fragment half4 basic_fragment() {
-    return half4(1.0);
+fragment half4 basic_fragment(VertexOut interpolated [[stage_in]]) {
+    return half4(interpolated.color[0],interpolated.color[1],interpolated.color[2],interpolated.color[3]);
 }
--- MetalSwift/ViewController.swift
+++ MetalSwift/ViewController.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2017 Danny. All rights reserved.
 //
 
-//https://www.raywenderlich.com/146414/metal-tutorial-swift-3-part-1-getting-started
+//https://www.raywenderlich.com/146416/metal-tutorial-swift-3-part-2-moving-3d
 
 /**
  In this tutorial, you’ll get some hands-on experience using Metal and Swift to create a bare-bones app: drawing a simple triangle. In the process, you’ll learn about some of the most important classes in Metal, such as devices, command queues, and more.
@@ -18,12 +18,16 @@ import Metal
 class ViewController: UIViewController {
     var device:MTLDevice!
     var metalLayer: CAMetalLayer!
-    let vertexData:[Float] = [0.0,1.0,0.0,-1.0,-1.0,0.0,1.0,-1.0,0.0]
-    var vertexBuffer:MTLBuffer!
+//    let vertexData:[Float] = [0.0,1.0,0.0,-1.0,-1.0,0.0,1.0,-1.0,0.0]
+//    var objectToDraw:Triangle!
+    var objectToDraw:Cube!
+
     var pipelineState : MTLRenderPipelineState!
     var commandQueue: MTLCommandQueue!
     var timer:CADisplayLink!
     
+    var projectionMatrix:Matrix4!
+    var lastFrameTimestamp: CFTimeInterval = 0.0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -35,9 +39,12 @@ class ViewController: UIViewController {
         metalLayer.frame = view.layer.frame
         view.layer.addSublayer(metalLayer)
         
-        let dataSize = vertexData.count * MemoryLayout.size(ofValue: vertexData[0])
-        vertexBuffer = device.makeBuffer(bytes: vertexData, length: dataSize, options: [])
+        objectToDraw = Cube(device: device)
         
+        
+        projectionMatrix = Matrix4.makePerspectiveViewAngle(Matrix4.degrees(toRad: 85.0), aspectRatio: Float(view.bounds.width/view.bounds.height), nearZ: 0.01, farZ: 100.0)
+        
+
         let defaultLibrary = device.newDefaultLibrary()
         let fragmentProgram = defaultLibrary?.makeFunction(name: "basic_fragment")
         let vertexProgram = defaultLibrary?.makeFunction(name: "basic_vertex")
@@ -51,35 +58,39 @@ class ViewController: UIViewController {
         
         commandQueue = device.makeCommandQueue()
         
-        timer = CADisplayLink(target: self, selector: #selector(ViewController.gameloop))
+        timer = CADisplayLink(target: self, selector: #selector(ViewController.newFrame(displayLink:)))
         timer.add(to: RunLoop.main, forMode: .defaultRunLoopMode)
+        
+
     }
     
-    func render(){
-        guard let drawable = metalLayer?.nextDrawable()  else {
-            return
+    func newFrame(displayLink:CADisplayLink) {
+        if lastFrameTimestamp == 0.0{
+            lastFrameTimestamp = displayLink.timestamp
         }
-        let renderPassDescriptor = MTLRenderPassDescriptor()
-        renderPassDescriptor.colorAttachments[0].texture = drawable.texture
-        renderPassDescriptor.colorAttachments[0].loadAction = .clear
-        renderPassDescriptor.colorAttachments[0].clearColor = MTLClearColor(red: 0.0, green: 104.0/255, blue: 5.0/255.0, alpha: 1.0)
         
-        let commandBuffer = commandQueue.makeCommandBuffer()
+        let elapsed : CFTimeInterval = displayLink.timestamp - lastFrameTimestamp
+        lastFrameTimestamp = displayLink.timestamp
         
-        let renderEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: renderPassDescriptor)
-        renderEncoder.setRenderPipelineState(pipelineState)
-        renderEncoder.setVertexBuffer(vertexBuffer, offset: 0, at: 0)
-        renderEncoder.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: 3,instanceCount:1)
-        renderEncoder.endEncoding()
-        
-        commandBuffer.present(drawable)
-        commandBuffer.commit()
-        
+        gameloop(timeSinceLastUpdate: elapsed)
     }
-    func gameloop() {
+
+    func gameloop(timeSinceLastUpdate:CFTimeInterval) {
+        objectToDraw.updateWithDelta(delta: timeSinceLastUpdate)
         autoreleasepool{
             self.render()
         }
+    }
+    
+    func render() {
+        guard let drawable = metalLayer?.nextDrawable() else {
+            return
+        }
+        
+        let worldModelMatrix = Matrix4()
+        worldModelMatrix.translate(0.0, y: 0.0, z: -7.0)
+        worldModelMatrix.rotateAroundX(Matrix4.degrees(toRad: 25), y: 0.0, z: 0.0)
+        objectToDraw.render(commandQueue: commandQueue, pipelineState: pipelineState, drawable: drawable,parentModelViewMatrix: worldModelMatrix, projectionMatrix: projectionMatrix,clearColor: nil)
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
